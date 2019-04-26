ec5743b1b0f665276a8c866f76bdc2b2f71d672f
Rémi Faucheux <faucheux@adobe.com>
2019-04-26 11:23:54 +0200

    ADD front price range search

--- ui.apps/src/main/content/jcr_root/apps/aem-template/clientlibs/clientlib-site/js/search.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-template/clientlibs/clientlib-site/js/search.js
@@ -1,10 +1,12 @@
-$(function () {    
+$(function () {
     $('.search-area-input').on('keyup', function () {
-    	var search = $('.search-area-input').val();
-    	console.info(search);
-    	$.get("/content/cultura/fr/resultat-de-la-recherche/jcr:content/root/responsivegrid/product_grid_magento.html?search=" + search, function( data ) {
-    		$(".product-grid-magento").html(data);
-    	});
+        var search = $('.search-area-input').val();
+        var priceMin = $("#slider-snap-input-lower").val();
+        var priceMax = $("#slider-snap-input-upper").val();
+        console.info(search + " " + priceMin + " " + priceMax);
+        $.get("/content/cultura/fr/resultat-de-la-recherche/jcr:content/root/responsivegrid/product_grid_magento.html?search=" +
+              search + "&priceMin=" + priceMin + "&priceMax=" + priceMax, function (data) {
+            $(".product-grid-magento").html(data);
+        });
     });
-    
 });
