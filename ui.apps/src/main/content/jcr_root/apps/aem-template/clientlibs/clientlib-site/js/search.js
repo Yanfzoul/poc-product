9fe2d15047334eb7e14df3b60086d6b68324f6eb
Rémi Faucheux <faucheux@adobe.com>
2019-04-29 09:15:04 +0200

    UPDATE: Search by categories

--- ui.apps/src/main/content/jcr_root/apps/aem-template/clientlibs/clientlib-site/js/search.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-template/clientlibs/clientlib-site/js/search.js
@@ -10,16 +10,28 @@ $(function () {
             });
     });
 
-	
 
     $(document).on('change', '.categoriesFacets input', function () {
         var str = $("form").serialize();
         var search = $('.search-area-input').val();
         var priceMin = $("#slider-snap-input-lower").val();
         var priceMax = $("#slider-snap-input-upper").val();
+
+        var categoriesSave = $('.categoriesFacets input').map(function() {
+            var param = "catValue=" + $(this).attr("data") + "&catId=" + $(this).val();
+            if ($(this).is(':checked'))
+                param += "&catCheck=true"
+            else
+				 param += "&catCheck=false"
+            return param;
+
+        }).get().join('&');
+
         console.info(search + " " + priceMin + " " + priceMax + " " + str);
+
 		$.get("/content/cultura/fr/resultat-de-la-recherche/jcr:content/root/responsivegrid/product_grid_magento.html?search=" +
-            search + "&priceMin=" + priceMin + "&priceMax=" + priceMax + "&" + str , function (data) {
+            search + "&priceMin=" + priceMin + "&priceMax=" + priceMax + "&" + categoriesSave + "&" + str, function (data) {
+
                 $(".product-grid-magento").html(data);
             });
     });
